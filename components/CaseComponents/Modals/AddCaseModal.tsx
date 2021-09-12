54427ecaf5421fec9092fac285a399d8c2a87595
Wesley Osogo <wesleyosogo@college.harvard.edu>
2021-09-11 23:15:21 -0400

    Yesss

--- components/CaseComponents/Modals/AddCaseModal.tsx
+++ components/CaseComponents/Modals/AddCaseModal.tsx
@@ -45,11 +45,10 @@ type AddCaseModalProps = {
   in this variable 
 */
 const InsertCaseMutation = `
-mutation AddCaseMutation($description: String = "", $name: String = "", $status: String = "", $category_id: String = "") {
+mutation AddCaseMutation($description: String = "", $name: String = "", $status: String = "") {
   insert_cases_one(object: {description: $description, name: $name, status: $status}) {
     name
     description
-    category_id
     status
   }
 }
